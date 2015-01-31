5ceb7caa75fffbe597a9c62c1f354a893f772c56
rezwang <rezwan.ghassemi@gmail.com>
2015-01-31 13:58:57 -0500

    .\loadDS.py

--- loadDS.py
+++ loadDS.py
@@ -6,6 +6,9 @@ from sklearn import datasets
 iris = datasets.load_iris()
 digits = datasets.load_digits()
 
+# digits.data gives access to the features that can be used to classify the digits samples
 print(digits.data)
-digits.target
-digits.images[0]
+
+#digits.target gives the ground truth for the digit dataset
+print(digits.target)
+print(digits.images[0])
